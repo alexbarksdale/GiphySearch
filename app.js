018755af23cb7c88e3934f2e71c8cf21fc9f4dc9
alexbarksdale <ale.barksdale@gmail.com>
2019-11-24 20:15:01 -0800

    [Add] Search functionality

--- app.js
+++ app.js
@@ -1,33 +1,44 @@
-const input = 'cat';
-const apiKey = 'KJRMEI6A4vMVZp1Rdzn4h9BcKWciiSR6';
-const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${input}`;
+const resultsElement = document.getElementById('results');
 
-fetch(path)
-    // Handles response
-    .then((res) => {
-        return res.json();
-    })
-    // Data that is loaded
-    .then((json) => {
-        const resultsElement = document.getElementById('results');
-        let resultsHTML = '';
+function search(input) {
+    const apiKey = 'KJRMEI6A4vMVZp1Rdzn4h9BcKWciiSR6';
+    const path = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${input}`;
 
-        json.data.forEach((obj) => {
-            const url = obj.images.fixed_width.url;
-            const height = obj.images.fixed_width.height;
-            const width = obj.images.fixed_width.width;
-            const title = obj.title;
+    fetch(path)
+        // Handles response
+        .then((res) => {
+            return res.json();
+        })
+        // Data that is loaded
+        .then((json) => {
+            let resultsHTML = '';
 
-            resultsHTML += `
+            json.data.forEach((obj) => {
+                const url = obj.images.fixed_width.url;
+                const height = obj.images.fixed_width.height;
+                const width = obj.images.fixed_width.width;
+                const title = obj.title;
+
+                resultsHTML += `
             <img src="${url}" 
             width="${width}" 
             height="${height} 
             alt="${title}">
             </img>`;
-        });
+            });
 
-        resultsElement.innerHTML = resultsHTML;
-    })
-    .catch((err) => {
-        console.log(`Failed to load: ${err.message}`);
-    });
+            resultsElement.innerHTML = resultsHTML;
+        })
+        .catch((err) => {
+            console.log(`Failed to load: ${err.message}`);
+        });
+}
+
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = searchInput.value;
+    search(input);
+});
